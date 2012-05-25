0fcfa1cfb4e151275f208c9807ef4cbf61c77d35
Hagen Mahnke <hagenmahnke@googlemail.com>
2012-05-25 17:30:05 +0200

    Add authentification

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,10 +28,11 @@ describe "Authentication" do
 
   	describe "with valid information" do
   	  let(:user) { FactoryGirl.create(:user) }
-  	  before { valid_signin(user) }
+  	  before { sign_in user }
 
   	  it { should have_title(user.name) }
   	  it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
   	  it { should have_link('Sign out', href: signout_path) }
   	  it { should_not have_link('Sign in', href: signin_path) }
 
@@ -40,5 +41,54 @@ describe "Authentication" do
         it { should have_link('Sign in') }
       end
   	end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed_in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          valid_signin(user)
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_title('Edit user')
+          end
+        end
+      end
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify {response.should redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_title(full_title('Edit user')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before {put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -61,5 +61,43 @@ describe "UserPages" do
         it { should have_link('Sign out') }
       end
     end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_h1("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",     with: new_name
+        fill_in "Email",    with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirm password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title new_name }
+      it { should have_success_message 'Profile updated' }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+    end
   end 
 end
